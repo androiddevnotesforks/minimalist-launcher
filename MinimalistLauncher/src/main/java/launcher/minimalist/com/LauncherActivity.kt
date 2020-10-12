6f931c527835fcf7cbfc8cb3219b024e269a140b
Joshua McWilliams <jtmacnb@gmail.com>
2020-10-12 10:39:20 -0500

    build out view model for managing favorite app for homescreen

--- MinimalistLauncher/src/main/java/launcher/minimalist/com/LauncherActivity.kt
+++ MinimalistLauncher/src/main/java/launcher/minimalist/com/LauncherActivity.kt
@@ -3,22 +3,29 @@ package launcher.minimalist.com
 import android.content.Intent
 import android.content.pm.ResolveInfo
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.ListItem
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.gesture.Direction
 import androidx.compose.ui.gesture.DragObserver
 import androidx.compose.ui.gesture.dragGestureFilter
+import androidx.compose.ui.gesture.longPressGestureFilter
+import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.viewModel
+import androidx.lifecycle.ViewModel
 import java.util.*
 
 class LauncherActivity : AppCompatActivity() {
@@ -39,19 +46,21 @@ class LauncherActivity : AppCompatActivity() {
 
 @Composable
 private fun ShowBodyContent(screenState: MutableState<LauncherScreen>, launcherActivity: LauncherActivity) {
+    val homeViewModel: HomeViewModel = viewModel(HomeViewModel::class.java)
+
     when (screenState.value) {
         LauncherScreen.HOME -> {
-            HomeContent(screenState = screenState)
+            HomeContent(screenState = screenState, launcherActivity = launcherActivity, homeViewModel = homeViewModel)
         }
         LauncherScreen.DRAWER -> {
-            DrawerContent(screenState = screenState, launcherActivity = launcherActivity)
+            DrawerContent(screenState = screenState, launcherActivity = launcherActivity, homeViewModel = homeViewModel)
         }
     }
 
 }
 
 @Composable
-fun HomeContent(screenState: MutableState<LauncherScreen>) {
+fun HomeContent(screenState: MutableState<LauncherScreen>, launcherActivity: LauncherActivity, homeViewModel: HomeViewModel) {
     val dragObserver: DragObserver = object : DragObserver {
         override fun onDrag(dragDistance: Offset): Offset {
             if (dragDistance.x < 10f) {
@@ -61,14 +70,30 @@ fun HomeContent(screenState: MutableState<LauncherScreen>) {
         }
     }
 
-    Column(modifier = Modifier.fillMaxSize()
+    Box(modifier = Modifier.fillMaxSize()
             .dragGestureFilter(dragObserver = dragObserver, canDrag = { it == Direction.LEFT })) {
-        Text(text = "Home!")
+
+        Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight(1 / 2f).align(Alignment.Center)) {
+            Column(modifier = Modifier.padding(horizontal = 30.dp)) {
+                homeViewModel.favoriteApps.forEach {
+                    Text(
+                            text = it.appName,
+                            style = MaterialTheme.typography.h4,
+                            modifier = Modifier
+                                    .clickable(
+                                            onClick = {
+                                                launcherActivity.startActivity(launcherActivity.packageManager.getLaunchIntentForPackage(it.packageName))
+                                            })
+                                    .padding(vertical = 16.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun DrawerContent(screenState: MutableState<LauncherScreen>, launcherActivity: LauncherActivity) {
+fun DrawerContent(screenState: MutableState<LauncherScreen>, launcherActivity: LauncherActivity, homeViewModel: HomeViewModel) {
     val dragObserver: DragObserver = object : DragObserver {
         override fun onDrag(dragDistance: Offset): Offset {
             if (dragDistance.x < 10f) {
@@ -79,12 +104,29 @@ fun DrawerContent(screenState: MutableState<LauncherScreen>, launcherActivity: L
     }
     val appList by remember { mutableStateOf(mutableListOf<LauncherApplication>()) }
     fetchAppList(appList, launcherActivity)
-    LazyColumnFor(items = appList, modifier = Modifier.fillMaxWidth()
-            .dragGestureFilter(dragObserver = dragObserver, canDrag = { it == Direction.RIGHT })) {
-        ListItem(modifier = Modifier.fillMaxWidth().clickable(onClick = {
-            launcherActivity.startActivity(launcherActivity.packageManager.getLaunchIntentForPackage(it.packageName))
-        })) {
-            Text(text = it.appName)
+
+    val context = ContextAmbient.current
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumnFor(items = appList, modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .dragGestureFilter(
+                        dragObserver = dragObserver,
+                        canDrag = {
+                            it == Direction.RIGHT
+                        }
+                )) {
+            ListItem(modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable(onClick = {
+                        launcherActivity.startActivity(launcherActivity.packageManager.getLaunchIntentForPackage(it.packageName))
+                    })
+                    .longPressGestureFilter { _: Offset ->
+                        homeViewModel.addFavoriteApp(it)
+                        Toast.makeText(context, "Favorite Added", Toast.LENGTH_SHORT).show()
+                    }) {
+                Text(text = it.appName)
+            }
         }
     }
 }
@@ -113,4 +155,15 @@ data class LauncherApplication(val appName: String, val packageName: String)
 
 enum class LauncherScreen {
     HOME, DRAWER
+}
+
+class HomeViewModel : ViewModel() {
+
+    var favoriteApps: List<LauncherApplication> by mutableStateOf(listOf())
+
+    //TODO Store in DB
+    fun addFavoriteApp(app: LauncherApplication) {
+        favoriteApps = favoriteApps + listOf(app)
+    }
+
 }
